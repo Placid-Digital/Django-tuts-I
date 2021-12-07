e6a610f7be32a9c396fda52bad345ff9e3f4c3f8
satish <satishchoudhary394@gmail.com>
2021-12-07 18:34:27 +0530

    worked on signals

--- Django-bank-project/short/urls.py
+++ Django-bank-project/short/urls.py
@@ -6,6 +6,9 @@ from . import views
 
 urlpatterns = [
     # path('', views.home, name="homepage"),
+    path('profile/', views.profile, name="profile"),
+    path('register/', views.register, name="create"),
+    path('login/', views.login, name="login")
 
 
 ]
--- Django-bank-project/short/views.py
+++ Django-bank-project/short/views.py
@@ -1,7 +1,11 @@
+import logging
+
+from django.contrib.auth import user_logged_in, signals
+from django.dispatch import receiver
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .forms import UserRegisterForm, ProfileUpdateForm, UserUpdateForm
 
 
 def register(request):
@@ -10,33 +14,47 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created! You are now able to log in')
+            messages.success(
+                request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html', {'form': form})
+    context = {
+        'form': form
+    }
+    return render(request, 'users/register.html', context)
 
 
-@login_required
+def login(request, *args, **kwargs):
+  res = login(request, *args, **kwargs)
+  if request.user.is_authenticated():
+    signals.user_login.send(sender=login, request=request, user=request.user)
+  return res
+
+
+#
+# @receiver(user_logged_in)
+# def user_logged_in(sender, user, request, **kwargs):
+#     logger = logging.getLogger(__name__)
+#     logger.info("user logged in: %s at %s" % (user, request.META['REMOTE_ADDR']))
+
+
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.user.profile)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
             messages.success(request, f'Your account has been updated!')
             return redirect('profile')
-
     else:
+
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
-
     context = {
         'u_form': u_form,
         'p_form': p_form
     }
-
     return render(request, 'users/profile.html', context)
