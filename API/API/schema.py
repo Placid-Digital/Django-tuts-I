de0cf7b0626e8e0dbb6cff9f8e90d180c71b5fbd
satish <satishchoudhary394@gmail.com>
2021-12-16 18:30:33 +0530

    rigapi

---
+++ API/API/schema.py
@@ -0,0 +1,20 @@
+import graphene
+
+from graphql_auth.schema import UserQuery, MeQuery
+from graphql_auth import mutations
+
+
+class AuthMutation(graphene.ObjectType):
+    register = mutations.Register.Field()
+
+
+class Mutation(AuthMutation, graphene.ObjectType):
+    pass
+
+
+class Query(UserQuery, MeQuery, graphene.ObjectType):
+    pass
+
+
+# schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
